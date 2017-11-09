f2a6d617577cf930fa46f4b91a5aee2c8a4c65a3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-09 15:57:38 -0500

    mosaicing

--- change_units.py
+++ change_units.py
@@ -1,30 +1,23 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  8 07:27:15 2017
-
-@author: hph
-"""
-
+import radio_beam
 from astropy.io import fits
 import numpy as np
 import os
 import glob
 
+corrected_hdus = []
 
 for filename in glob.glob(os.path.expanduser("~/Dropbox/SMA_CMZ/CMZoom_Images/November17_continuum_fits/G*fits")):
-    originalfits = fits.open('/Users/hph/current_fits/'+filename)
-    originaldata = originalfits[0].data[0,0,:,:]
+    originalfits = fits.open(filename)
+    originaldata = originalfits[0].data.squeeze()
     originalheader = originalfits[0].header
-    bmaj = originalheader['BMAJ']
-    bmin = originalheader['BMIN']
-    beam_area_naive = np.pi*bmaj*bmin
-    conversion_factor = (41253./(4*np.pi))*((2.35)**2)/beam_area_naive
-    
-    newdata = originaldata*conversion_factor
-    
-    newfits = originalfits
-    newfits[0].data = newdata
-    
-    newfits.writeto('/Users/hph/current_fits/new_units/'+filename+'mod_Jy_per_Ster.fits',overwrite=True)
+    beam = radio_beam.Beam.from_fits_header(originalheader)
 
+    assert originalheader['BUNIT'] == 'Jy/beam'
+    
+    newdata = originaldata / beam.sr.value
+    
+    hdu = fits.PrimaryHDU(data=newdata, header=originalheader)
+
+    corrected_hdus.append(hdu)
+
+#outheader = ...
